ea291150ce21280edbf6b07e8dffd8f45abcd8e4
rohitravibhat <65624931+rohitravibhat@users.noreply.github.com>
2023-02-12 00:39:46 -0600

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,4 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "Hello, bigWorld! php version is " . PHP_VERSION . "\n";
-print "Hello, bigWorldindia! php version is " . PHP_VERSION . "\n";
+
 ?>
