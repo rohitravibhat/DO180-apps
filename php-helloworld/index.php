cca38e4e5ee94e74ef52d1d5bfa0d668cb910420
rohitravibhat <65624931+rohitravibhat@users.noreply.github.com>
2024-11-12 10:03:32 -0500

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,5 @@
 <?php
 print "Hello, World ....! php version is " . PHP_VERSION . "\n";
 print "Hello, BITS ....! php version is " . PHP_VERSION . "\n";
+print "Hello, class ....! php version is " . PHP_VERSION . "\n";
 ?>
