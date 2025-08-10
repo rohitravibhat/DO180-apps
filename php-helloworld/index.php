f40786a3b78e885fc6fcbfb91a158bfcacb4eb4b
Student User <student@workstation.lab.example.com>
2025-08-10 05:28:26 -0400

    first change

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,6 +1,4 @@
 <?php
 print "Hello, World ....! php version is " . PHP_VERSION . "\n";
-print "Hello, BITS ....! php version is " . PHP_VERSION . "\n";
-print "Hello, all....! php version is " . PHP_VERSION . "\n";
 
 ?>
